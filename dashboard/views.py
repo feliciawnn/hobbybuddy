8a914119eec47318a160ea00346fd41d78cca3d7
Michael Shane <msha0080@student.monash.edu>
2021-07-18 14:21:35 +0700

    Implement dashboard personalized content

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from activity.models import Activity
+from activity.models import Activity, ActivityCategory, UserCategory
 
 
 # Create your views here.
 @login_required(login_url="/")
 def index(request):
-    context = {'first': request.user.first_name, 'last': request.user.last_name, 'activities': Activity.objects.all()}
+    context = {'first': request.user.first_name, 'last': request.user.last_name}
+    user_category = UserCategory.objects.filter(user=request.user)
+    preferred_category = []
+    for uc in user_category:
+        preferred_category.append(uc.category)
+
+    preferred_activity = ActivityCategory.objects.filter(category__in=preferred_category)
+    activities = []
+    for pa in preferred_activity:
+        activities.append(pa.activity)
+    context['activities'] = activities
     return render(request, "dashboard/dashboard.html", context)
 
 
